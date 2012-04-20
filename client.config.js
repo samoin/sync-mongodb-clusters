f19214f1e3c04e41bdd46f23403084475904e118
samoin <gigi_ly180@sohu.com>
2012-04-20 15:21:27 +0800

    修改ppt

--- client.config.js
+++ client.config.js
@@ -11,7 +11,7 @@
 module.exports = {
 	server_port : 8081,
 	server_host : "127.0.0.1",
-	start_from_local_oplog_ts : true,
+	start_from_local_oplog_ts : false,
 	cluster_info : "mongodb://172.16.9.117:47017,172.16.9.117:47018,172.16.9.117:47019",//
 	secure_key : {name : "ty2" , key : "1234567890ty2"},
 	sync_db_namespace : ""
